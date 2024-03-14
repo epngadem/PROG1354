6e36ebe9eaa0ac0dc56520a2262cde99bca7f3b5
epngadem <epngadem01@monccnb.ca>
2024-03-14 11:46:05 -0300

    implementation de la classe cable

---
+++ src/cable.cpp
@@ -0,0 +1,14 @@
+#include "Cable.h"
+#include <Arduino.h>
+
+Cable::Cable() {
+    // Définir les broches d'entrée et de sortie
+    int inputPins[8] = {15, 2, 4, 5, 18, 19, 21, 22};
+    int outputPins[8] = {13, 14, 27, 26, 25, 33, 32, 12};
+
+    // Copier les broches d'entrée et de sortie dans les tableaux de la classe
+    for (int i = 0; i < 8; i++) {
+        pinsInput[i] = inputPins[i];
+        pinsOutput[i] = outputPins[i];
+    }
+}
